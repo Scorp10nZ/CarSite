91ed9f4b9dc19088636ec7ba69df897b3b2fc8d3
Scorp10nZ <adriano.pinto.2004@gmail.com>
2024-02-08 16:08:06 +0000

    .

--- Project01Site/index.php
+++ Project01Site/index.php
@@ -48,42 +48,42 @@ require 'phpconfig/config.php';
     </header>
     <!-- Section-->
     <section class="py-5">
-    <div class="container px-4 px-lg-5 mt-5">
-        <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-            <div class="col mb-5">
+        <div class="container px-4 px-lg-5 mt-5">
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 <?php
                 $query = "SELECT * FROM carros";
                 $result = mysqli_query($conn, $query);
                 if (mysqli_num_rows($result) > 0) {
                     while ($row = mysqli_fetch_assoc($result)) {
                 ?>
-                        <div class="card h-60">
-                            <!-- Product image -->
-                            <img class="card-img-top" src="imagens/<?php echo $row['imagem']; ?>" alt="" />
-                            <!-- Product details -->
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <!-- Product name -->
-                                    <h5 class="fw-bolder"><?php echo $row['Modelo']; ?></h5>
-                                    <!-- Product price -->
-                                    <?php echo $row['Preco']; ?>
+                        <div class="col mb-5">
+                            <form action="index.php?action=add&id=<?php echo $row["id"] ?>" method="post">
+                                <div class="card h-60">
+                                    <img class="card-img-top" src="imagens/<?php echo $row['imagem']; ?>" width="300px" height="200px" />
+                                    <!-- Product details -->
+                                    <div class="card-body p-4">
+                                        <div class="text-center">
+                                            <!-- Product name -->
+                                            <h5 class="fw-bolder"><?php echo $row['Modelo']; ?></h5>
+                                            <!-- Product price -->
+                                            <?php echo $row['Preco']; ?>€
+                                        </div>
+                                    </div>
+                                    <!-- Product actions -->
+                                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                                    </div>
                                 </div>
-                            </div>
-                            <!-- Product actions -->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
-                            </div>
+                            </form>
+                           
                         </div>
-                <?php
-                    }
-                } else {
-                    echo "Nenhum carro encontrado.";
-                }
-                ?>
+                        <?php
+                            }
+                        }          
+                        ?>
             </div>
         </div>
-    </div>
-</section>
+    </section>
     <!-- Footer-->
     <footer class="py-5 bg-dark">
         <div class="container">
