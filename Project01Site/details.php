de23165008b67fa2804954c42e79e214fb5a1368
Scorp10nZ <adriano.pinto.2004@gmail.com>
2024-02-08 18:04:25 +0000

    .

--- Project01Site/details.php
+++ Project01Site/details.php
@@ -13,26 +13,55 @@ if (isset($_GET['id'])) {
     if (mysqli_num_rows($result) > 0) {
         $car = mysqli_fetch_assoc($result);
 ?>
+
         <!DOCTYPE html>
         <html lang="en">
 
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="css/styles.css" rel="stylesheet" />
             <title>Detalhes do Carro</title>
         </head>
 
-        <body>
+        <body style="background-color:#f1f1f1;">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container px-4 px-lg-5">
+                    <a class="navbar-brand" href="#!">CarDreamer</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                            <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
+                        </ul>
+                        <form class="d-flex">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                                <li class="nav-item"><a class="nav-link" href="Register.php">Register</a></li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </nav>
             <div class="container">
                 <h1>Detalhes do Carro</h1>
                 <div>
+                    <h1><?php echo $car['Marca']; ?></h1>
                     <h2><?php echo $car['Modelo']; ?></h2>
                     <p>Preço: <?php echo $car['Preco']; ?>€</p>
+                    <p>Ano: <?php echo $car['Ano']; ?> </p>
+                    <p>Kilometragem: <?php echo $car['kilometragem']; ?> </p>
+                    <p>Combustivel: <?php echo $car['combustivel']; ?></p>
+                    <p>Cavalagem: <?php echo $car['cavalagem']; ?></p>
+                    <p>Cilindrada: <?php echo $car['cilindrada']; ?></p>
+                    <p>Tipo de caixa: <?php echo $car['tipo_de_caixa']; ?></p>
+                    <p>Tracção: <?php echo $car['tracção']; ?></p>
                 </div>
             </div>
         </body>
 
         </html>
+
+
+
 <?php
     } else {
         echo "Carro não encontrado.";
