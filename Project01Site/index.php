505cf3fbed285dcc0d5e00dcff8a2ec1fe9ab019
Scorp10nZ <adriano.pinto.2004@gmail.com>
2024-02-08 15:37:43 +0000

    .

--- Project01Site/index.php
+++ Project01Site/index.php
@@ -47,7 +47,7 @@ require 'phpconfig/config.php';
         </div>
     </header>
     <!-- Section-->
-    <section class="py-5">
+    <section class="">
     <div class="container px-4 px-lg-5 mt-5">
         <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
             <div class="col mb-5">
@@ -57,7 +57,7 @@ require 'phpconfig/config.php';
                 if (mysqli_num_rows($result) > 0) {
                     while ($row = mysqli_fetch_assoc($result)) {
                 ?>
-                        <div class="card h-100">
+                        <div class="card h-60">
                             <!-- Product image -->
                             <img class="card-img-top" src="imagens/<?php echo $row['imagem']; ?>" alt="" />
                             <!-- Product details -->
