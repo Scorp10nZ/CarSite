da4ee33568f6e43113fbe132781925d92d48c916
Scorp10nZ <adriano.pinto.2004@gmail.com>
2024-02-09 00:53:24 +0000

    .

--- Project01Site/Login.php
+++ Project01Site/Login.php
@@ -39,10 +39,10 @@ if(isset($_POST["submit"])){
     <section class="vh-100" style="background-color:gray;">
         <div class="container h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-lg-12 col-xl-11">
+                <div class="col-lg-10 col-xl-9">
                     <div class="card text-black" style="border-radius: 25px;">
                         <div class="card-body p-md-5">
-                            <div class="row justify-content-center">
+                            <div class="row justify-content-center FIT">
                                 <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
                                     <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Login</p>
@@ -79,8 +79,8 @@ if(isset($_POST["submit"])){
                                     </form>
 
                                 </div>
-                                <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                                    <img src="imagens/wp10500951.webp" alt="" class="FIT" width="600" height="600">
+                                <div class="col-md-12 col-lg-6 col-xl-7 d-flex align-items-center order-1 ">
+                                    <img src="imagens/wp10500951.webp" alt="" class="FIT">
                                 </div>
                             </div>
                         </div>
--- Project01Site/Register.php
+++ Project01Site/Register.php
@@ -44,13 +44,13 @@ if (isset($_POST['submit'])) {
     <section class="vh-100" style="background-color:gray;">
         <div class="container h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-lg-12 col-xl-11">
+                <div class="col-lg-11 col-xl-9">
                     <div class="card text-black" style="border-radius: 25px;">
                         <div class="card-body p-md-5">
                             <div class="row justify-content-center">
                                 <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+                                    <p class="text-center h1 fw-bold mb-2 mx-1 mx-md-3 ">Sign up</p>
 
                                     <form class="mx-1 mx-md-4">
 
@@ -86,12 +86,11 @@ if (isset($_POST['submit'])) {
                                                 <input class="form-control" type="password" name="confirmpassword" id="confirmpassword" required />
                                             </div>
                                         </div>
-
-                                        <div class="form-check d-flex justify-content-center mb-5">
+                                        <div class="form-check d-flex justify-content-center mb-3">
                                                  <a href="Login.php">Já tem uma conta</a>
                                         </div>
 
-                                        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                        <div class="mx-4 mb-4 mb-lg-1" style="padding-left:30%">
                                             <button type="submit" class="btn btn-dark btn-lg">Register</button>
                                         </div>
 
