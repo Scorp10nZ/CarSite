b68f09562f6e7db992bf9b2a0728f1a4391f69f8
Scorp10nZ <adriano.pinto.2004@gmail.com>
2024-02-10 23:56:00 +0000

    .

--- Project01Site/details.php
+++ Project01Site/details.php
@@ -1,48 +1,50 @@
 <?php
 require 'phpconfig/config.php';
 
-if (isset($_GET['id'])) {
-    if (!$conn) {
-        die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
-    }
-
-    $car_id = $_GET['id'];
-    $query = "SELECT * FROM carros WHERE id = $car_id";
-    $result = mysqli_query($conn, $query);
-
-    if (mysqli_num_rows($result) > 0) {
-        $car = mysqli_fetch_assoc($result);
 ?>
 
-        <!DOCTYPE html>
-        <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-            <link href="css/styles.css" rel="stylesheet" />
-            <title>Detalhes do Carro</title>
-        </head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link href="css/styles.css" rel="stylesheet" />
+    <title>Detalhes do Carro</title>
+</head>
 
-        <body style="background-color:lightslategray">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div class="container px-4 px-lg-5">
-                    <a class="navbar-brand" href="#!">CarDreamer</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                            <?php if (isset($_GET['username'])) { ?>
-                                <a class="nav-link active" aria-current="page" href="index2.php?username=<?php echo urlencode($_SESSION['username']); ?>">Home</a>
-                            <?php } else { ?>
-                                <a class="nav-link active" aria-current="page" href="index2.php">Home</a>
-                            <?php } ?>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            <div class="container ">
+<body style="background-color:lightslategray">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="#!">CarDreamer</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                    <?php if (isset($_GET['username'])) { ?>
+                        <a class="nav-link active" aria-current="page" href="index2.php?username=<?php echo urlencode($_SESSION['username']); ?>">Home</a>
+                    <?php } else { ?>
+                        <a class="nav-link active" aria-current="page" href="index2.php">Home</a>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container ">
+        <?php
+        if (isset($_GET['id'])) {
+            if (!$conn) {
+                die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
+            }
+
+            $car_id = $_GET['id'];
+            $query = "SELECT * FROM carros WHERE id = $car_id";
+            $result = mysqli_query($conn, $query);
+
+            if (mysqli_num_rows($result) > 0) {
+                $car = mysqli_fetch_assoc($result);
+        ?>
                 <div class="row">
                     <div class="col-md-6" style="margin-top: 2%;">
                         <h2 style="color: white; padding-left:25%"> <?php echo $car['Marca']; ?> <?php echo $car['Modelo']; ?></h2>
@@ -67,71 +69,71 @@ if (isset($_GET['id'])) {
                         </div>
                     </div>
                 </div>
-            </div>
-            <div style="margin-left: 60%; margin: botton 10%;" >
-                                <button type="button" class="btn btn-secondary btn-lg">Contatar Vendedor</button>
-                    </div>
-            <div class="py-3 bg-dark" style="margin-top: 10% ;color:white">
-                <h3 class="m-0 text-center">Mais Anuncios</h3>
-                <section class="py-5">
-                    <div class="container px-4 px-lg-5 mt-5">
-                        <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                            <?php
-                            $query = "SELECT * FROM carros WHERE id!=$car_id ORDER BY RAND()";
-                            $result = mysqli_query($conn, $query);
-                            $count = 0;
-                            if (mysqli_num_rows($result) > 0) {
-                                while ($row = mysqli_fetch_assoc($result)) {
-                                    if ($count >= 4) {
-                                        break;
-                                    }
-                            ?>
-                                    <div class="col mb-5">
-                                        <form action="index.php?action=add&id=<?php echo $row["id"] ?>" method="post">
-                                            <div class="card h-60">
-                                                <img class="card-img-top" src="imagens/<?php echo $row['imagem']; ?>" width="300px" height="200px" />
-                                                <!-- Product details -->
-                                                <div class="card-body p-4">
-                                                    <div class="text-center">
-                                                        <!-- Product name -->
-                                                        <h5 class="fw-bolder"><?php echo $row['Modelo']; ?></h5>
-                                                        <!-- Product price -->
-                                                        <?php echo $row['Preco']; ?>€
-                                                    </div>
-                                                </div>
-                                                <!-- Product actions -->
-                                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="details.php?id=<?php echo $row["id"]; ?>">More details</a></div>
-                                                </div>
-                                            </div>
-                                        </form>
-
-                                    </div>
-                            <?php
-                                    $count++;
-                                }
+    </div>
+    <div style="margin-left: 60%; margin: botton 10%;">
+        <button type="button" class="btn btn-secondary btn-lg">Contatar Vendedor</button>
+    </div>
+    <div class="py-3 bg-dark" style="margin-top: 10% ;color:white">
+        <h3 class="m-0 text-center">Mais Anuncios</h3>
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <?php
+                    $query = "SELECT * FROM carros WHERE id!=$car_id ORDER BY RAND()";
+                    $result = mysqli_query($conn, $query);
+                    $count = 0;
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            if ($count >= 4) {
+                                break;
                             }
-                            ?>
-                        </div>
-                    </div>
-                </section>
-            </div>
-        </body>
-        <footer class="py-3 bg-dark">
-            <div class="container">
-                <p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p>
-            </div>
-        </footer>
+                    ?>
+                            <div class="col mb-5">
+                                <form action="index.php?action=add&id=<?php echo $row["id"] ?>" method="post">
+                                    <div class="card h-60">
+                                        <img class="card-img-top" src="imagens/<?php echo $row['imagem']; ?>" width="300px" height="200px" />
+                                        <!-- Product details -->
+                                        <div class="card-body p-4">
+                                            <div class="text-center">
+                                                <!-- Product name -->
+                                                <h5 class="fw-bolder"><?php echo $row['Modelo']; ?></h5>
+                                                <!-- Product price -->
+                                                <?php echo $row['Preco']; ?>€
+                                            </div>
+                                        </div>
+                                        <!-- Product actions -->
+                                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                            <div class="text-center"> <a class="btn btn-outline-dark mt-auto" href="details.php?id=<?php echo $row["id"]; ?><?php if(isset($_GET['username'])) echo '&username=' . urlencode($_GET['username']); ?>">More details</a></div>
+                                        </div>
+                                    </div>
+                                </form>
 
-        </html>
+                            </div>
+                    <?php
+                            $count++;
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+<footer class="py-3 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p>
+    </div>
+</footer>
+
+</html>
 
 
 
 <?php
-    } else {
-        echo "Carro não encontrado.";
-    }
-} else {
-    echo "ID do carro não especificado.";
-}
+            } else {
+                echo "Carro não encontrado.";
+            }
+        } else {
+            echo "ID do carro não especificado.";
+        }
 ?>
