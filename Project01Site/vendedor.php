fdb28c6d981598ffde515a16c7c0dfde2fc2e9b5
Scorp10nZ <adriano.pinto.2004@gmail.com>
2024-02-14 00:47:03 +0000

    .

--- Project01Site/vendedor.php
+++ Project01Site/vendedor.php
@@ -155,8 +155,10 @@ require 'phpconfig/config.php';
                 </div>
 
     </main>
-    <footer>
-        <!-- place footer here -->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2024</p>
+        </div>
     </footer>
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
